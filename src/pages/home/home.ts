41dc5b4fd1cd95567c22a70be3c751554f419ef2
BookieOandasan <bookieoandasan@gmail.com>
2018-09-05 11:41:40 -0700

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,23 +7,31 @@ import Feed from 'feed-to-json-promise'
   templateUrl: 'home.html'
 })
 export class HomePage {
-  dailyReading:any;
+  dailyReading:string;
+  dailyReadingTitle:string;
+  dailyReadingDate:Date;
+  showDailyReading:boolean = false;
 
   constructor(public navCtrl: NavController) {
     const feed = new Feed()
     feed.load('http://www.usccb.org/bible/readings/rss/index.cfm').then(feed => {
-      console.log(feed)
-      console.log(feed.items[0].description)
-      this.dailyReading = feed.items[0].description
-    }).catch(error => {
-      console.error(error)
+   
+      for (let index = 0; index <  feed.items.length; index++) {
+        const element =  feed.items[index];
+      
+        let currentDate:Date = new Date();
+        let readingDate:Date = new Date(element.date)
+        if(Date.UTC( readingDate.getFullYear(), readingDate.getMonth(), readingDate.getDate()) ==Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())){
+            this.dailyReading = feed.items[index].description
+            this.dailyReadingTitle = feed.items[index].title;
+            this.dailyReadingDate = feed.items[index].date;
+            console.log("Daily Reading Date: " + feed.items[index].date);
+
+          if (this.dailyReading==''){this.showDailyReading = false} else{this.showDailyReading = true}
+            }
+        
+      }}).catch(error => {       console.error(error)
     })
-
-
-    // feed.load('http://www.usccb.org/bible/readings/rss/index.cfm',
-    //        result=> {
-    //       this.dailyReading = result
-    //     })
 
   }
 
